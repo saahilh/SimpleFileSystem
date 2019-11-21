34ab8e66581f4d0c341882651ae96a67d5bd91b5
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2019-11-21 18:16:29 -0500

    Updated header function declarations to match C convention

--- sfs_api.h
+++ sfs_api.h
@@ -1,13 +1,19 @@
 //Returns -1 for errors besides mksfs
 
-void mksfs(int fresh);
-int sfs_fopen(char *name);
-int sfs_fclose(int fileID);
-int sfs_frseek(int fileID, int loc);
-int sfs_fwseek(int fileID, int loc);
-int sfs_fwrite(int fileID, char *buf, int length);
-int sfs_fread(int fileID, char *buf, int length);
-int sfs_remove(char *file);
+void 	mksfs				(int fresh);
+int 	sfs_fopen		(char *name);
+int 	sfs_fclose	(int fileID);
+int 	sfs_frseek	(int fileID, 
+									 int loc);
+int 	sfs_fwseek	(int fileID, 
+									 int loc);
+int 	sfs_fwrite	(int fileID, 
+									 char *buf, 
+									 int length);
+int 	sfs_fread		(int fileID, 
+									 char *buf, 
+									 int length);
+int 	sfs_remove	(char *file);
 
 #include <stdio.h>
 #include <stdlib.h>
